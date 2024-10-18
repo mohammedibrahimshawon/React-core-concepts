1e584f983652467455df1311cdf998b50fd7f576
mohammedibrahimshawon <mohammedibrahimshawonaiub@gmail.com>
2024-10-18 06:58:25 +0600

    prams

--- explore-react-core-concepts-part2/src/App.jsx
+++ explore-react-core-concepts-part2/src/App.jsx
@@ -4,12 +4,22 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  function handleClick(){
+    alert('button clicked');
+  }
+  function handleClick2(){
+    alert('button 2 clicked')
+  }
 
   return (
     <>
      
       <h1>core  React part 2 </h1>
+      <button onClick={handleClick}>click me </button>
+      <button onClick={handleClick2}>Click2</button>
+      <button onClick={()=>{alert('third clicked')}}>Button 3</button>
+
+
      
     </>
   )
